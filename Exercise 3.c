33e7eedb3fa2d58c26f59f31cce68bd303e70def
Quinuel Tabot  N-A <97244109+QUIN04@users.noreply.github.com>
2023-01-24 20:52:30 +0100

    Add files via upload

---
+++ Exercise 3.c
@@ -0,0 +1,25 @@
+DOCTYPE<html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewspot" content="width=device-width">
+    <title>GIDEON AND GILLIAN</title>
+    <style>
+          strong{
+              color:green;
+              font-style:oblique;
+          }
+     </style>
+</head>
+<body>
+    <a href="Google/about.html"download>about gideon</a>
+    <a href="mailto:programlngwithngwesegideon@gmail/com>
+    <p>I LOVE TO TEACH YOU<strong>HTML</strong></p>
+    <h1><strong><u>WHO IS GIDEON</u></strong></h1>
+    <P>GIDEON is any man made resourse which is scarce in natures
+    <h2>WHAT IS FOOD</h2>
+    <p>food is something eatable</p>
+    <h3>WHAT IS LIFE</U></h3>
+    <p>Idont know ask your self!</p>
+</body>
+</html>
