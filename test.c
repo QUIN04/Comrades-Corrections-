aab1e49888a87a89a6ee2708b1fbe89c9319b89d
Quinuel Tabot  N-A <97244109+QUIN04@users.noreply.github.com>
2023-02-01 22:01:01 +0100

    Add files via upload

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int a, b,c,d;
+	 int x ,y ,ax,cx,by,dy ;
+	//the constant and variables above will be used in solving the two set of equations by method of substition.
+    	printf("enter a number:");
+	scanf("%d", &a);
+		printf("enter a number:");
+	scanf("%d", &b);
+		printf("enter a number:");
+	scanf("%d", &c);
+		printf("enter a number:");
+	scanf("%d", &d);
+		ax + by == 0;
+	    cx + dy == 0;
+	    return 0;
+}
